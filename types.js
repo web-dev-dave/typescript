0909792f8ce789eb1c3fd91acfad4eff2ad9fd6e
David Steel <webdevsteel@gmail.com>
2021-02-22 10:46:24 +1300

    types

--- types.js
+++ types.js
@@ -1,1 +1,27 @@
-console.log('Hello from ts land!');
+// Simple variable types
+var myString;
+var myNum;
+var myBool;
+var myVar;
+// Has to be array of...
+var strArr;
+var numArr;
+var boolArr;
+// Alternate 
+// let strArr: Array<string>
+// let numArr: Array<number>
+// let boolArr: Array<boolean>
+// Tuples - an array with a fixed set of declared elements
+var strNumTuple;
+myString = 'Hello World';
+myNum = 22;
+myBool = true;
+myVar = 'Hello';
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+strNumTuple = ['Hello', 6];
+var myVoid = null;
+var myNull = null;
+var myUndefined = undefined;
+console.log(myNull);
--- types.ts
+++ types.ts
@@ -1,1 +1,36 @@
-console.log('Hello from ts land!')
+// Simple variable types
+let myString: string
+let myNum: number
+let myBool: boolean
+let myVar: any
+
+
+// Has to be array of...
+let strArr: string[]
+let numArr: number[]
+let boolArr: boolean[]
+
+// Alternate 
+// let strArr: Array<string>
+// let numArr: Array<number>
+// let boolArr: Array<boolean>
+
+// Tuples - an array with a fixed set of declared elements
+let strNumTuple: [string, number]
+
+myString = 'Hello World'
+myNum = 22
+myBool = true
+myVar = 'Hello'
+
+strArr = ['Hello', 'World']
+numArr = [1, 2 ,3]
+boolArr = [true, false, true]
+
+strNumTuple = ['Hello', 6]
+
+let myVoid: void = null
+let myNull: null = null
+let myUndefined: undefined = undefined
+
+console.log(myNull)
